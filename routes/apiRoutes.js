87bfc32fbe076915169157c34d7344c97bfe9db3
Demi Wu <demiwu96@gmail.com>
2020-06-24 19:34:48 -0400

    OK

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const booksController = require("../controller/bookController");
 
-router.get("/api/savedbooks",booksController.findAll);
+router.get("/savedbooks",booksController.findAll);
 
-router.post("/api/books",booksController.create);
+router.post("/books",booksController.create);
 
-router.delete("/api/books/:id",booksController.remove);
+router.delete("/books/:id",booksController.remove);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
   })
 }
 // Add routes, both API and view
-app.use(routes);
+app.use("/api",routes);
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/googleBook", { useNewUrlParser: true });
